bbaaedfe1a2a4147f0e93fafd4c3fb9542ea35ae
mblack <martin@parkopedia.com>
2020-10-28 13:45:11 +0000

    Also install git

--- boostrap.sh
+++ boostrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Installing sudo, make & ansible, and allow user \"${USER}\" to run any command with sudo..."
+echo "Installing sudo, git, make & ansible, and allow user \"${USER}\" to run any command with sudo..."
 
-LOCAL_USER=${USER} su -c 'apt-get install sudo make ansible && echo "${LOCAL_USER}      ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/${LOCAL_USER}'
+LOCAL_USER=${USER} su -c 'apt-get install sudo git make ansible && echo "${LOCAL_USER}      ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/${LOCAL_USER}'
