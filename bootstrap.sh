64368416b18156958c43875c967308c34f072e90
mblack <martin@parkopedia.com>
2020-10-28 13:52:13 +0000

    Simplify bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "Installing sudo, git, make & ansible, and allow user \"${USER}\" to run any command with sudo..."
-
-LOCAL_USER=${USER} su -c 'apt-get install sudo git make ansible && echo "${LOCAL_USER}      ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/${LOCAL_USER}'
+echo "Installing make and ansible"
+sudo apt update && sudo apt install make ansible
